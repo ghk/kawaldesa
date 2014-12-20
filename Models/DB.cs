d76a641b7961eaa6ca5b7daf21ccdad7066523ad
Vidyapati <vidyapati@microvac.co.id>
2014-12-20 17:24:52 +0700

    change favicon

--- Models/DB.cs
+++ Models/DB.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Data.Entity.Validation;
+using System.Web;
 
 namespace App.Models
 {
