f418546fa0fcf83958f17b209fe40c6bdffbff5c
Gozali Kumara <ghk@gozalikumara.com>
2015-08-28 05:03:59 +0700

    restructure index ctrl

--- Scripts/KawalDesa/Controllers/IndexCtrl.ts
+++ Scripts/KawalDesa/Controllers/IndexCtrl.ts
@@ -49,29 +49,18 @@ module App.Controllers {
 
     export class IndexCtrl {
 
-        regionTree: Models.Region[];
         region: Models.Region;
+        regionId: string;
+        regionTree: Models.Region[];
+
         childName: string;
         type = "transfer";
-        currentUser: ICurrentUser;
-        regionId: string;
         guessedRegionType: number;
+
         isPathReplacing = false;
         currentPath = null;
 
-        activeUploadType: App.Models.DocumentUploadType;
-        activeUploadRegionId: string;
-        activeUpload: App.Models.Spreadsheet;
-        newUploadFile: any;
-        newUpload: App.Models.Spreadsheet;
-        newUploadState = false;
-
-        activeSources:  App.Models.SourceDocument[];
-        newSourceFile: any;
-        newSourceSubType: any;
-        newSourceFunction: any;
-        newSourceRegion: any;
-        newSourceState = false;
+        currentUser: ICurrentUser;
 
         static $inject = ["$scope", "$location"];
 
@@ -91,7 +80,6 @@ module App.Controllers {
                 ctrl.isPathReplacing = false;
             });
         }
-
 
         onLocationChange() {
             var path = this.$location.path();
@@ -128,8 +116,23 @@ module App.Controllers {
             this.guessedRegionType = this.guessType(regionId);
             this.regionId = regionId;
             this.currentPath = path;
+        }
 
+        onSearchSelected(item, model, label) {
+            console.log(model);
+            var regionId = model.Type == 4 ? model.ParentId : model.Id;
+            var type = this.type;
+            var matched : any[] = ROUTES.filter(r => r[1] == type);
+            var path = matched[0][0] + regionId;
+            this.$location.path(path);
+        }
 
+        guessType(regionId: string) {
+            if (regionId == "0")
+                return 0;
+            if (!regionId)
+                return null;
+            return (regionId.match(/\./g) || []).length + 1;
         }
 
         changeType(type, $event) {
@@ -159,23 +162,64 @@ module App.Controllers {
             this.$location.path(path);
         }
 
-        searchRegions(keyword) {
-            return Controllers.RegionSearchResultController.GetAll({ "keyword": keyword });
+        loadRegion(parentId?: string, parentKey?: string) {
+            setTimeout(() => {
+                ctrl.$scope.$apply(() => {
+                this.$scope.$broadcast("regionChangeBefore");
+                });
+            }, 0);
+            var ctrl = this;
+
+            this.regionTree = [];
+            this.childName = CHILD_NAMES[0];
+
+            var promise = null;
+            if (parentId != null)
+                promise = Controllers.RegionController.Get(parentId);
+            else if (parentKey)
+                promise = Controllers.RegionController.GetByURLKey(parentKey);
+
+            promise.done((region: Models.Region) => {
+                ctrl.$scope.$apply(() => {
+                    ctrl.region = region;
+                    ctrl.regionId = region.Id;
+
+                    ctrl.initSourceUploadDefaults();
+
+                    var regionTree = [];
+                    var cur : Models.IRegion = region;
+                    while (cur) {
+                        regionTree.push(cur);
+                        cur = cur.Parent;
+                    }
+                    ctrl.regionTree = regionTree.reverse();
+                    if (regionTree.length < CHILD_NAMES.length)
+                        ctrl.childName = CHILD_NAMES[regionTree.length];
+
+                    setTimeout(() => {
+                        ctrl.$scope.$apply(() => {
+                            ctrl.$scope.$broadcast("regionChangeSuccess");
+                        });
+                    }, 0);
+
+                    if (region.UrlKey && ctrl.$location.path() != "/" + region.UrlKey) {
+                        ctrl.isPathReplacing = true;
+                        ctrl.$location.path("/" + region.UrlKey);
+                        ctrl.$location.replace();
+                    }
+                });
+            });
         }
 
-        onSearchSelected(item, model, label) {
-            console.log(model);
-            var regionId = model.Type == 4 ? model.ParentId : model.Id;
-            var type = this.type;
-            var matched : any[] = ROUTES.filter(r => r[1] == type);
-            var path = matched[0][0] + regionId;
-            this.$location.path(path);
+        /* UI Utils */
+
+        modal(selector, action) {
+            var modal: any = $(selector);
+            modal.modal(action);
         }
 
-        hasAnyVolunteerRoles() {
-            return this.currentUser != null
-                && this.currentUser.Roles.some(r => r.indexOf("volunteer_") != -1);
-        }
+
+        /* Security Utils */
 
         isInRole(roleName) {
             if (!this.currentUser) {
@@ -194,24 +238,66 @@ module App.Controllers {
             return this.isInRole(roleName) && this.isInScope(entityId);
         }
 
-        modal(selector, action) {
-            var modal: any = $(selector);
-            modal.modal(action);
+        hasAnyVolunteerRoles() {
+            return this.currentUser != null
+                && this.currentUser.Roles.some(r => r.indexOf("volunteer_") != -1);
         }
+
+
+        /* Google Sheet */
+
+        isLoadingUrl = false;
+
+        openGoogleSheet() {
+            if (this.isLoadingUrl)
+                return;
+            var ctrl = this;
+            this.isLoadingUrl = true;
+            Controllers.SpreadsheetController.GetCurrentSheetUrl(this.activeUploadType, this.activeUploadRegionId, "2015p").done(url => {
+                ctrl.$scope.$apply(() => {
+                    ctrl.isLoadingUrl = false;
+                    window.open(url, "_blank");
+                });
+            });
+        }
+
+
+        /* Search */
+
+        showSearch() {
+            this.$scope.searchShown = true;
+            setTimeout(function () {
+                $(".search-input-group input").focus();
+                $(".search-input-group input").select();
+            }, 0);
+        }
+
+        searchRegions(keyword) {
+            return Controllers.RegionSearchResultController.GetAll({ "keyword": keyword });
+        }
+
+
+        /* Upload */
+
+        activeUploadType: App.Models.DocumentUploadType;
+        activeUploadRegionId: string;
+        activeUpload: App.Models.Spreadsheet;
+
+        activeSources:  App.Models.SourceDocument[];
+        newSourceFile: any;
+        newSourceSubType: any;
+        newSourceFunction: any;
+        newSourceRegion: any;
+        newSourceState = false;
 
         configureDocumentUpload(type: App.Models.DocumentUploadType, regionId: string) {
             this.activeUploadType = type;
             this.activeUploadRegionId = regionId;
-            this.newUpload = new Models.Spreadsheet();
             var ctrl = this;
             ctrl.activeUpload = null;
             Controllers.SpreadsheetController.GetActive(type, regionId, "2015p").done(doc => {
                 ctrl.$scope.$apply(() => {
                     ctrl.activeUpload = doc;
-                    if (doc) {
-                        ctrl.newUpload.DocumentName = doc.DocumentName;
-                        ctrl.newUpload.Notes = doc.Notes;
-                    }
                 });
             });
             Controllers.SourceDocumentController.GetAll({"fkRegionId": regionId, "type": type, "apbnKey": "2015p"}).done(sources => {
@@ -221,21 +307,22 @@ module App.Controllers {
             });
         }
 
-        upload() {
+        initSourceUploadDefaults() {
             var ctrl = this;
-            var multipart = new Scaffold.Multipart({ files: this.newUploadFile, forms: this.newUpload });
-            ctrl.newUploadState = true;
-            Controllers.SpreadsheetController.Upload(multipart, this.activeUploadType, this.activeUploadRegionId, "2015p").success(() => {
-                safeApply(ctrl.$scope, () => {
-                    ctrl.modal('#document-upload-modal', 'hide');
-                    ctrl.configureDocumentUpload(ctrl.activeUploadType, ctrl.activeUploadRegionId);
-                    ctrl.$scope.$broadcast("regionChangeSuccess");
-                });
-            }).finally(() => {
-                safeApply(ctrl.$scope, () => {
-                    ctrl.newUploadState = false;
-                });
-            });;
+            var region = ctrl.region;
+
+            ctrl.newSourceRegion = region;
+            if (region.Type == 1 || region.Type == 3)
+                ctrl.newSourceRegion = region.Parent;
+            if (region.Type == 4)
+                ctrl.newSourceRegion = region.Parent.Parent;
+            ctrl.newSourceFunction = ctrl.type == "transfer" ? "1" : "0";
+            if (ctrl.type == "dd")
+                ctrl.newSourceSubType = "Dd";
+            if (ctrl.type == "add")
+                ctrl.newSourceSubType = "Add";
+            if (ctrl.type == "bhpr")
+                ctrl.newSourceSubType = "Bhpr";
         }
 
         uploadSource() {
@@ -259,100 +346,6 @@ module App.Controllers {
             });;
         }
 
-        guessType(regionId: string) {
-            if (regionId == "0")
-                return 0;
-            if (!regionId)
-                return null;
-            return (regionId.match(/\./g) || []).length + 1;
-        }
-
-        loadRegion(parentId?: string, parentKey?: string) {
-            setTimeout(() => {
-                ctrl.$scope.$apply(() => {
-                this.$scope.$broadcast("regionChangeBefore");
-                });
-            }, 0);
-            var ctrl = this;
-
-            this.regionTree = [];
-            this.childName = CHILD_NAMES[0];
-
-            var promise = null;
-            if (parentId != null)
-                promise = Controllers.RegionController.Get(parentId);
-            else if (parentKey)
-                promise = Controllers.RegionController.GetByURLKey(parentKey);
-
-            promise.done((region: Models.Region) => {
-                ctrl.$scope.$apply(() => {
-                    ctrl.region = region;
-                    ctrl.regionId = region.Id;
-
-                    /* configure source upload defaults */
-                    ctrl.newSourceRegion = region;
-                    if (region.Type == 1 || region.Type == 3)
-                        ctrl.newSourceRegion = region.Parent;
-                    if (region.Type == 4)
-                        ctrl.newSourceRegion = region.Parent.Parent;
-                    ctrl.newSourceFunction = ctrl.type == "transfer" ? "1" : "0";
-                    if (ctrl.type == "dd")
-                        ctrl.newSourceSubType = "Dd";
-                    if (ctrl.type == "add")
-                        ctrl.newSourceSubType = "Add";
-                    if (ctrl.type == "bhpr")
-                        ctrl.newSourceSubType = "Bhpr";
-                    /* end configure source upload defaults */
-
-                    var regionTree = [];
-                    var cur : Models.IRegion = region;
-                    while (cur) {
-                        regionTree.push(cur);
-                        cur = cur.Parent;
-                    }
-                    ctrl.regionTree = regionTree.reverse();
-                    if (regionTree.length < CHILD_NAMES.length)
-                        ctrl.childName = CHILD_NAMES[regionTree.length];
-
-                    setTimeout(() => {
-                        ctrl.$scope.$apply(() => {
-                            ctrl.$scope.$broadcast("regionChangeSuccess");
-                        });
-                    }, 0);
-                    if (region.UrlKey && ctrl.$location.path() != "/" + region.UrlKey) {
-                        ctrl.isPathReplacing = true;
-                        ctrl.$location.path("/" + region.UrlKey);
-                        ctrl.$location.replace();
-                    }
-                });
-            });
-        }
-
-        /* Google Sheet */
-
-        isLoadingUrl = false;
-
-        openGoogleSheet() {
-            if (this.isLoadingUrl)
-                return;
-            var ctrl = this;
-            this.isLoadingUrl = true;
-            Controllers.SpreadsheetController.GetCurrentSheetUrl(this.activeUploadType, this.activeUploadRegionId, "2015p").done(url => {
-                ctrl.$scope.$apply(() => {
-                    ctrl.isLoadingUrl = false;
-                    window.open(url, "_blank");
-                });
-            });
-        }
-        /* End Google Sheet*/
-
-        showSearch() {
-            this.$scope.searchShown = true;
-            setTimeout(function () {
-                $(".search-input-group input").focus();
-                $(".search-input-group input").select();
-            }, 0);
-        }
 
     }
 
