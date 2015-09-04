35dc3f3bceb39123612eb47907c1bb82f9eff7cd
Gozali Kumara <ghk@gozalikumara.com>
2015-09-04 12:02:13 +0700

    add spreadsheet work item

---
+++ Models/SpreadsheetWorkItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace App.Models
+{
+    public class SpreadsheetWorkItem
+    {
+        public string GoogleSheetId { get; set; }
+
+        [ForeignKey("DeactivatedBy")]
+        public long fkUserId { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey("Region")]
+        public String fkRegionId { get; set; }
+        public virtual Region Region { get; set; }
+
+        public DocumentUploadType Type { get; set; }
+        public String ApbnKey { get; set; }
+    }
+}
