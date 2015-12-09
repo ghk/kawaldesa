dd2b9b01ce71f9cd477e53691bd65d530e24e1dc
Gozali Kumara <ghk@gozalikumara.com>
2015-12-10 02:05:23 +0700

    remove frozen sql

--- SQLs/dd/frozen_national_dd_recapitulations.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE MATERIALIZED VIEW frozen_national_dd_recapitulations AS
-        SELECT * from national_dd_recapitulations;
-		
-CREATE UNIQUE INDEX frozen_national_dd_recapitulations_IDX_id
-  ON frozen_national_dd_recapitulations (id); 
-CREATE INDEX frozen_national_dd_recapitulations_IDX_apbn_key_parent_region_id
-  ON frozen_national_dd_recapitulations (apbn_key, parent_region_id);
--- SQLs/dd/frozen_regional_dd_recapitulations.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE MATERIALIZED VIEW frozen_regional_dd_recapitulations AS
-        SELECT * from regional_dd_recapitulations;
-		
-CREATE UNIQUE INDEX frozen_regional_dd_recapitulations_IDX_id
-  ON frozen_regional_dd_recapitulations (id); 
-CREATE INDEX frozen_regional_dd_recapitulations_IDX_apbn_key_parent_region_id
-  ON frozen_regional_dd_recapitulations (apbn_key, parent_region_id);
